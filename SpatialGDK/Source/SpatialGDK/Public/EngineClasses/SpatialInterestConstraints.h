872f2d2a9fbd30d55dc39088b5191c39c93dacde
Tim Gibson <timgibson@improbable.io>
2019-06-26 10:33:33 -0600

    Implement constraints

--- SpatialGDK/Source/SpatialGDK/Public/EngineClasses/SpatialInterestConstraints.h
+++ SpatialGDK/Source/SpatialGDK/Public/EngineClasses/SpatialInterestConstraints.h
@@ -9,7 +9,6 @@
 
 namespace SpatialGDK
 {
-struct Query;
 struct QueryConstraint;
 }
 class USchemaDatabase;
@@ -19,140 +18,215 @@ class SPATIALGDK_API UAbstractQueryConstraint : public UObject
 {
 	GENERATED_BODY()
 public:
+	UAbstractQueryConstraint() = default;
+	virtual ~UAbstractQueryConstraint() = default;
+
 	virtual void CreateConstraint(const USchemaDatabase& SchemaDatabase, SpatialGDK::QueryConstraint& OutConstraint) const PURE_VIRTUAL(UAbstractQueryConstraint::CreateConstraint, );
 };
 
+/**
+ * Creates a constraint that is satisfied if any of its inner constraints are satisfied.
+ */
 UCLASS(BlueprintType, EditInlineNew, DefaultToInstanced)
-class SPATIALGDK_API UOrConstraint : public UAbstractQueryConstraint
+class SPATIALGDK_API UOrConstraint final : public UAbstractQueryConstraint
 {
 	GENERATED_BODY()
 public:
+	UOrConstraint() = default;
+	~UOrConstraint() = default;
+
 	virtual void CreateConstraint(const USchemaDatabase& SchemaDatabase, SpatialGDK::QueryConstraint& OutConstraint) const override;
 
 	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Instanced)
 	TArray<UAbstractQueryConstraint *> Constraints;
 };
 
+/**
+ * Creates a constraint that is satisfied if all of its inner constraints are satisfied.
+ */
 UCLASS(BlueprintType, EditInlineNew, DefaultToInstanced)
-class SPATIALGDK_API UAndConstraint : public UAbstractQueryConstraint
+class SPATIALGDK_API UAndConstraint final : public UAbstractQueryConstraint
 {
 	GENERATED_BODY()
 public:
+	UAndConstraint() = default;
+	~UAndConstraint() = default;
+
 	virtual void CreateConstraint(const USchemaDatabase& SchemaDatabase, SpatialGDK::QueryConstraint& OutConstraint) const override;
 
 	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Instanced)
 	TArray<UAbstractQueryConstraint *> Constraints;
 };
 
+/**
+ * Creates a constraint that includes all entities within a sphere centered on the specified point.
+ */
 UCLASS(BlueprintType, EditInlineNew, DefaultToInstanced)
-class SPATIALGDK_API USphereConstraint : public UAbstractQueryConstraint
+class SPATIALGDK_API USphereConstraint final : public UAbstractQueryConstraint
 {
 	GENERATED_BODY()
 public:
+	USphereConstraint() = default;
+	~USphereConstraint() = default;
+
 	virtual void CreateConstraint(const USchemaDatabase& SchemaDatabase, SpatialGDK::QueryConstraint& OutConstraint) const override;
 
 	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly)
-	FVector Center;
+	FVector Center = FVector::ZeroVector;
 
 	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Meta = (ClampMin = 0.0))
-	float Radius;
+	float Radius = 0.0f;
 };
 
+/**
+ * Creates a constraint that includes all entities within a cylinder centered on the specified point.
+ */
 UCLASS(BlueprintType, EditInlineNew, DefaultToInstanced)
-class SPATIALGDK_API UCylinderConstraint : public UAbstractQueryConstraint
+class SPATIALGDK_API UCylinderConstraint final : public UAbstractQueryConstraint
 {
 	GENERATED_BODY()
 public:
+	UCylinderConstraint() = default;
+	~UCylinderConstraint() = default;
+
 	virtual void CreateConstraint(const USchemaDatabase& SchemaDatabase, SpatialGDK::QueryConstraint& OutConstraint) const override;
 
 	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly)
-	FVector Center;
+	FVector Center = FVector::ZeroVector;
 
 	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Meta = (ClampMin = 0.0))
-	float Radius;
+	float Radius = 0.0f;
 };
 
+/**
+ * Creates a constraint that includes all entities within a bounding box centered on the specified point.
+ */
 UCLASS(BlueprintType, EditInlineNew, DefaultToInstanced)
-class SPATIALGDK_API UBoxConstraint : public UAbstractQueryConstraint
+class SPATIALGDK_API UBoxConstraint final : public UAbstractQueryConstraint
 {
 	GENERATED_BODY()
 public:
+	UBoxConstraint() = default;
+	~UBoxConstraint() = default;
+
 	virtual void CreateConstraint(const USchemaDatabase& SchemaDatabase, SpatialGDK::QueryConstraint& OutConstraint) const override;
 
 	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly)
-	FVector Center;
+	FVector Center = FVector::ZeroVector;
 
 	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Meta = (ClampMin = 0.0))
-	FVector EdgeLengths;
+	FVector EdgeLengths = FVector::ZeroVector;
 };
 
+/**
+ * Creates a constraint that includes all entities within a sphere centered on the actor.
+ */
 UCLASS(BlueprintType, EditInlineNew, DefaultToInstanced)
-class SPATIALGDK_API URelativeSphereConstraint : public UAbstractQueryConstraint
+class SPATIALGDK_API URelativeSphereConstraint final : public UAbstractQueryConstraint
 {
 	GENERATED_BODY()
 public:
+	URelativeSphereConstraint() = default;
+	~URelativeSphereConstraint() = default;
+
 	virtual void CreateConstraint(const USchemaDatabase& SchemaDatabase, SpatialGDK::QueryConstraint& OutConstraint) const override;
 
 	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Meta = (ClampMin = 0.0))
-	float Radius;
+	float Radius = 0.0f;
 };
 
+/**
+ * Creates a constraint that includes all entities within a cylinder centered on the actor.
+ */
 UCLASS(BlueprintType, EditInlineNew, DefaultToInstanced)
-class SPATIALGDK_API URelativeCylinderConstraint : public UAbstractQueryConstraint
+class SPATIALGDK_API URelativeCylinderConstraint final : public UAbstractQueryConstraint
 {
 	GENERATED_BODY()
 public:
+	URelativeCylinderConstraint() = default;
+	~URelativeCylinderConstraint() = default;
+
 	virtual void CreateConstraint(const USchemaDatabase& SchemaDatabase, SpatialGDK::QueryConstraint& OutConstraint) const override;
 
 	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Meta = (ClampMin = 0.0))
-	float Radius;
+	float Radius = 0.0f;
 };
 
+/**
+ * Creates a constraint that includes all entities within a bounding box centered on the actor.
+ */
 UCLASS(BlueprintType, EditInlineNew, DefaultToInstanced)
-class SPATIALGDK_API URelativeBoxConstraint : public UAbstractQueryConstraint
+class SPATIALGDK_API URelativeBoxConstraint final : public UAbstractQueryConstraint
 {
 	GENERATED_BODY()
 public:
+	URelativeBoxConstraint() = default;
+	~URelativeBoxConstraint() = default;
+
 	virtual void CreateConstraint(const USchemaDatabase& SchemaDatabase, SpatialGDK::QueryConstraint& OutConstraint) const override;
 
 	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Meta = (ClampMin = 0.0))
-	FVector EdgeLengths;
+	FVector EdgeLengths = FVector::ZeroVector;
 };
 
+/**
+ * Creates a constraint that includes an actor type (including subtypes) and a cylindrical range around the actor with the interest.
+ */
 UCLASS(BlueprintType, EditInlineNew, DefaultToInstanced)
-class SPATIALGDK_API UCheckoutRadiusConstraint : public UAbstractQueryConstraint
+class SPATIALGDK_API UCheckoutRadiusConstraint final : public UAbstractQueryConstraint
 {
 	GENERATED_BODY()
 public:
-	virtual void CreateConstraint(const USchemaDatabase& SchemaDatabase, SpatialGDK::QueryConstraint& OutConstraint) const override;
+	UCheckoutRadiusConstraint() = default;
+	~UCheckoutRadiusConstraint() = default;
 
-	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Meta = (ClampMin = 0.0))
-	float Radius;
+	virtual void CreateConstraint(const USchemaDatabase& SchemaDatabase, SpatialGDK::QueryConstraint& OutConstraint) const override;
 
 	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly)
 	TSubclassOf<AActor> ActorClass;
+
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Meta = (ClampMin = 0.0))
+	float Radius = 0.0f;
 };
 
+/**
+ * Creates a constraint that includes all actors of a type (optionally including subtypes).
+ */
 UCLASS(BlueprintType, EditInlineNew, DefaultToInstanced)
-class SPATIALGDK_API UActorClassConstraint : public UAbstractQueryConstraint
+class SPATIALGDK_API UActorClassConstraint final : public UAbstractQueryConstraint
 {
 	GENERATED_BODY()
 public:
+	UActorClassConstraint() = default;
+	~UActorClassConstraint() = default;
+
 	virtual void CreateConstraint(const USchemaDatabase& SchemaDatabase, SpatialGDK::QueryConstraint& OutConstraint) const override;
 
 	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly)
 	TSubclassOf<AActor> ActorClass;
+
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly)
+	bool bIncludeDerivedClasses = true;
 };
 
+/**
+ * Creates a constraint that includes all components of a type (optionally including subtypes).
+ */
 UCLASS(BlueprintType, EditInlineNew, DefaultToInstanced)
-class SPATIALGDK_API UComponentClassConstraint : public UAbstractQueryConstraint
+class SPATIALGDK_API UComponentClassConstraint final : public UAbstractQueryConstraint
 {
 	GENERATED_BODY()
 public:
+	UComponentClassConstraint() = default;
+	~UComponentClassConstraint() = default;
+
 	virtual void CreateConstraint(const USchemaDatabase& SchemaDatabase, SpatialGDK::QueryConstraint& OutConstraint) const override;
 
 	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly)
 	TSubclassOf<UActorComponent> ComponentClass;
+
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly)
+	bool bIncludeDerivedClasses = true;
 };
 
 
